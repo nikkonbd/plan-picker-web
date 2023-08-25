e3528762a5fc32396d508733190a846c0fde5c0a
himangsu roy <himur98@gmail.com>
2023-08-25 13:30:38 +0600

    added new dashboard and responsive and alignment navigation rote components

--- src/myCalendar/createEvent/CreateEvent.jsx
+++ src/myCalendar/createEvent/CreateEvent.jsx
@@ -1,114 +1,10 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-
-// const CreateEventButton = () => {
-//   const [isOpen, setIsOpen] = useState(false);
-//   const [selectedOption, setSelectedOption] = useState(null);
-
-//   const toggleDropdown = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   const handleOptionSelect = (option) => {
-//     setSelectedOption(option);
-//     setIsOpen(false);
-//     // Perform any action based on the selected option here
-//   };
-
-//   return (
-//     <div className="relative">
-//       <div className="">
-//         <Link>
-//           <button
-//             type="button"
-//             className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-//             onClick={toggleDropdown}>
-//             <span className="mr-2">➕</span> Add Event
-//           </button>
-//         </Link>
-//       </div>
-//       {isOpen && (
-//         <div className="absolute right-0 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 w-80">
-//           <div
-//             className="py-2"
-//             role="menu"
-//             aria-orientation="vertical"
-//             aria-labelledby="options-menu">
-//             <button
-//               onClick={() =>
-//                 handleOptionSelect({
-//                   // name: "One-on-One",
-//                   // icon: "📅",
-//                   // title: "One host with one invitee",
-//                   // description: "Create a new event on the calendar.",
-//                 })
-//               }
-//               className="block w-full px-4 py-3 text-sm text-left text-gray-700 hover:bg-gray-100"
-//               role="menuitem">
-//               <span className="mr-2 text-lg">📅</span>
-//               <div className="flex flex-col">
-//                 <span className="text-lg font-bold text-black">One-on-One</span>
-//                 <span className="text-base"><b>One host</b> with <b>one invitee</b></span>
-//                 <span className="text-base-700 gray-500">
-//                   Good for: 1:1 interview, chats, etc.
-//                 </span>
-//               </div>
-//             </button>
-//             <button
-//               onClick={() =>
-//                 handleOptionSelect({
-//                   // name: "One-on-One",
-//                   // icon: "📅",
-//                   // title: "One host with one invitee",
-//                   // description: "Create a new event on the calendar.",
-//                 })
-//               }
-//               className="block w-full px-4 py-3 text-sm text-left text-gray-700 hover:bg-gray-100"
-//               role="menuitem">
-//               <span className="mr-2 text-lg">📅</span>
-//               <div className="flex flex-col">
-//                 <span className="text-lg font-bold text-black">Group</span>
-//                 <span className="text-base"><b>One host</b> with <b>group of invitee</b> </span>
-//                 <span className="text-base-700 gray-500">
-//                   Good for: webinars, online classes, etc.
-//                 </span>
-//               </div>
-//             </button>
-//             <button
-//               onClick={() =>
-//                 handleOptionSelect({
-//                   // name: "One-on-One",
-//                   // icon: "📅",
-//                   // title: "One host with one invitee",
-//                   // description: "Create a new event on the calendar.",
-//                 })
-//               }
-//               className="block w-full px-4 py-3 text-sm text-left text-gray-700 hover:bg-gray-100"
-//               role="menuitem">
-//               <span className="mr-2 text-lg">📅</span>
-//               <div className="flex flex-col">
-//                 <span className="text-lg font-bold text-black">Collective</span>
-//                 <span className="text-base"><b>More than one host</b> with <b>one invitee</b></span>
-//                 <span className="text-base-700 gray-500">
-//                   Good for: panel interview, group sales calls, etc.
-//                 </span>
-//               </div>
-//             </button>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default CreateEventButton;
-
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const CreateEventButton = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
+  const navigate = useNavigate();
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -117,7 +13,14 @@ const CreateEventButton = () => {
   const handleOptionSelect = (option) => {
     setSelectedOption(option);
     setIsOpen(false);
-    // Perform any action based on the selected option here
+
+    if (option.name === "One-on-One") {
+      navigate("/dashboard/event_type/one-on-one-form");
+    } else if (option.name === "Group") {
+      navigate("/dashboard/event_type/group-form");
+    } else if (option.name === "Collective") {
+      navigate("/dashboard/event_type/collective-form");
+    }
   };
 
   return (
@@ -142,7 +45,10 @@ const CreateEventButton = () => {
             <button
               onClick={() =>
                 handleOptionSelect({
-                  // ... your option data ...
+                  name: "One-on-One",
+                  icon: "📅",
+                  title: "One host with one invitee",
+                  description: "Create a new event on the calendar.",
                 })
               }
               className="block w-full px-4 py-3 text-sm text-left text-gray-700 hover:bg-gray-100"
@@ -168,7 +74,10 @@ const CreateEventButton = () => {
             <button
               onClick={() =>
                 handleOptionSelect({
-                  // ... your option data ...
+                  name: "Group",
+                  icon: "📅",
+                  title: "One host with one invitee",
+                  description: "Create a new event on the calendar.",
                 })
               }
               className="block w-full px-4 py-3 text-sm text-left text-gray-700 hover:bg-gray-100"
@@ -193,7 +102,10 @@ const CreateEventButton = () => {
             <button
               onClick={() =>
                 handleOptionSelect({
-                  // ... your option data ...
+                  name: "Collective",
+                  icon: "📅",
+                  title: "One host with one invitee",
+                  description: "Create a new event on the calendar.",
                 })
               }
               className="block w-full px-4 py-3 text-sm text-left text-gray-700 hover:bg-gray-100"
@@ -223,3 +135,4 @@ const CreateEventButton = () => {
 };
 
 export default CreateEventButton;
+
