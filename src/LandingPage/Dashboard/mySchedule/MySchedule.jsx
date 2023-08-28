96dcd7eb1639daea831933a3dc414a1da7cf53fe
opu183059 <opuctg440@gmail.com>
2023-08-29 00:15:19 +0600

    My Schedule page added

--- src/LandingPage/Dashboard/mySchedule/MySchedule.jsx
+++ src/LandingPage/Dashboard/mySchedule/MySchedule.jsx
@@ -1,9 +1,84 @@
 import React from "react";
+import ScheduleCard from "./ScheduleCard";
 
 const MySchedule = () => {
+  const today = new Date();
+  const date =
+    today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+
+  const todayScedule = [
+    {
+      id: 11,
+      title: "Interview of company X",
+      description: "Demo text for the description",
+      address: "Bangladesh",
+      date: "01-09-2023",
+      start_time: "11 am",
+      end_time: "2 pm",
+      expected_perticipents: 12,
+      total_host: 3,
+    },
+  ];
+  const schedule = [
+    {
+      id: 1,
+      title: "Interview of company X",
+      description: "Demo text for the description",
+      address: "Bangladesh",
+      date: "06-09-2023",
+      start_time: "11 am",
+      end_time: "2 pm",
+      expected_perticipents: 12,
+      total_host: 3,
+    },
+    {
+      id: 2,
+      title: "Interview of company Y",
+      description: "Demo text for the description",
+      address: "Dhaka",
+      date: "08-09-2023",
+      start_time: "11am",
+      end_time: "2pm",
+      expected_perticipents: 12,
+      total_host: 1,
+    },
+    {
+      id: 3,
+      title: "Interview of company Y",
+      description: "Demo text for the description",
+      address: "Dhaka",
+      date: "11-09-2023",
+      start_time: "11am",
+      end_time: "2pm",
+      expected_perticipents: 12,
+      total_host: 1,
+    },
+  ];
   return (
     <div>
-      <h1>My schedule</h1>
+      <h2 className="text-2xl">Today ({date})</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 my-5">
+        {todayScedule.map((scheduleData) => (
+          <ScheduleCard
+            key={scheduleData.id}
+            scheduleData={scheduleData}
+          ></ScheduleCard>
+        ))}
+      </div>
+      <h2 className="text-2xl relative">
+        Upcomming
+        <span className="bg-orange-600 animate-pulse text-white px-2 py-1 text-xs rounded-full absolute">
+          {schedule.length}
+        </span>
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 my-5">
+        {schedule.map((scheduleData) => (
+          <ScheduleCard
+            key={scheduleData.id}
+            scheduleData={scheduleData}
+          ></ScheduleCard>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/LandingPage/Dashboard/mySchedule/ScheduleCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { RxCross2 } from "react-icons/rx";
+const ScheduleCard = ({ scheduleData }) => {
+  const {
+    id,
+    title,
+    description,
+    address,
+    date,
+    start_time,
+    end_time,
+    expected_perticipents,
+    total_host,
+  } = scheduleData || {};
+  return (
+    <div className="relative border-[1px] border-teal-500 bg-gradient-to-tl from-teal-50/30 via-teal-100/30 to-teal-200/30 p-8 rounded-lg shadow hover:shadow-xl cursor-default">
+      <button className="bg-red-500 text-white p-1 rounded-full absolute -right-2 -top-2">
+        {" "}
+        <RxCross2 size={18}></RxCross2>
+      </button>
+      <p className="mb-4">
+        Schedule Date:{" "}
+        <span className="font-semibold text-lg text-teal-900">{date}</span>
+      </p>
+      <h3 className="text-xl font-bold text-teal-800">{title}</h3>
+      <h4 className="mb-1">{description}</h4>
+      <p className="pb-2 border-b-[1px] border-teal-700">{address}</p>
+
+      <div className="flex justify-between mt-5">
+        <p>Start Time: {start_time}</p>
+        <p>End Time: {end_time}</p>
+      </div>
+      <div className="flex justify-between">
+        <p>Host: {total_host}</p>
+        <p>
+          Perticipents:{" "}
+          <span className="text-teal-700 font-bold">
+            {expected_perticipents}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ScheduleCard;
