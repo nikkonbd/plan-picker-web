00204ed8432151c29d700538d09b238e5a1b3775
himangsu roy <himur98@gmail.com>
2023-08-28 18:59:52 +0600

    initial redux toolkit and rich text editor bug fixed

--- src/LandingPage/contact/Event.jsx
+++ src/LandingPage/contact/Event.jsx
@@ -3,14 +3,52 @@ import { BiInfoCircle } from "react-icons/bi";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { submitFormData } from "../../store/features/formSubmission/formSubmissionSlice";
 
 const Event = () => {
-  const [value, setValue] = useState("");
   const navigate = useNavigate();
+  const [value, setValue] = useState("");
+  const [location, setLocation] = useState(""); 
+  const [eventName, setEventName] = useState(""); 
+  const [eventLink, setEventLink] = useState(""); 
+  const [eventColor, setEventColor] = useState("");
+  const [sanitizedValue, setSanitizedValue] = useState("");
   
+  const dispatch = useDispatch();
+  const { isLoading, isSuccess, error } = useSelector(
+    (state) => state.formSubmission
+  );
+  
+
+  const handleChange = (html) => {
+    const sanitizedText = removePTags(html);
+    setValue(html);
+    setSanitizedValue(sanitizedText);
+  };
+
   const handleNextForm = () => {
+    // Store the data in an object
+    const formData = {
+      eventName,
+      location,
+      description: sanitizedValue,
+      eventLink,
+      eventColor,
+    };
+    
+    dispatch(submitFormData(formData));
+
+    console.log(formData); // Display the form data
     navigate("/dashboard/one-on-one-form/event_set_edit_form");
-  }
+  };
+  
+    // Utility function to remove <p> tags
+  const removePTags = (content) => {
+    const div = document.createElement("div");
+    div.innerHTML = content;
+    return div.textContent || div.innerText || "";
+  };
 
   return (
     <div className="py-6 mt-10 md:px-10">
@@ -25,30 +63,41 @@ const Event = () => {
         </div>
         <div className="flex justify-center gap-4 md:justify-between">
           <button className="p-2 rounded-md btn">Cancel</button>
-          <button onClick={() => handleNextForm()} className="px-2 rounded-md btn btn-primary ">Next</button>
+          <button
+            onClick={() => handleNextForm()}
+            className="px-2 rounded-md btn btn-primary ">
+            Next
+          </button>
         </div>
       </div>
       <div className="divider"></div>
       <form className="mt-6">
+        
         <div className="w-full max-w-md form-control">
           <label className="label">
             <span className="flex items-center gap-3 font-bold label-text">
-              Event Name * <BiInfoCircle></BiInfoCircle>
+              Event Name * <BiInfoCircle />
             </span>
           </label>
           <input
             type="text"
             placeholder="Event Name"
             className="w-full max-w-md font-semibold input input-bordered"
+            value={eventName}
+            onChange={(e) => setEventName(e.target.value)}
           />
         </div>
         <div className="w-full max-w-md form-control">
           <label className="label">
             <span className="flex items-center gap-3 font-bold label-text">
-              Location <BiInfoCircle></BiInfoCircle>
+              Location <BiInfoCircle />
             </span>
           </label>
-          <select className="select select-bordered">
+          <select
+            className="select select-bordered"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          >
             <option disabled selected className="text-gray-200">
               Add Location
             </option>
@@ -66,29 +115,29 @@ const Event = () => {
             </span>
           </label>
           <ReactQuill
-            theme="snow"
             value={value}
-            onChange={setValue}
-            className="h-[300px] "
+            onChange={handleChange}
+            className="h-[300px]"
           />
-          ;
         </div>
         <div className="w-full mt-10 form-control md:max-w-4xl">
           <label className="label">
             <span className="flex items-center gap-3 font-bold label-text">
-              Event Link * <BiInfoCircle></BiInfoCircle>
+              Event Link * <BiInfoCircle />
             </span>
           </label>
           <p className="label ">PlanPicker.com</p>
           <input
             type="text"
             className="w-full max-w-md font-semibold input input-bordered"
+            value={eventLink}
+            onChange={(e) => setEventLink(e.target.value)}
           />
         </div>
         <div className="w-full max-w-2xl mt-10 form-control">
           <label className="label">
             <span className="flex items-center gap-3 font-bold label-text">
-              Event Color * <BiInfoCircle></BiInfoCircle>
+              Event Color * <BiInfoCircle />
             </span>
           </label>
           <div className="flex gap-1">
@@ -99,14 +148,19 @@ const Event = () => {
             <div className="bg-yellow-500 rounded-full w-7 h-7"></div>
           </div>
         </div>
-      </form>
-      <div className="divider"></div>
-      <div className="flex justify-center md:justify-end">
-        <div className="flex gap-4 ">
-          <button className="p-2 rounded-md btn">Cancel</button>
-          <button onClick={() => handleNextForm()} className="px-2 rounded-md btn btn-primary ">Next</button>
+        <div className="divider"></div>
+        <div className="flex justify-center md:justify-end">
+          <div className="flex gap-4 ">
+            <button className="p-2 rounded-md btn">Cancel</button>
+            <button
+              onClick={() => handleNextForm()}
+              className="px-2 rounded-md btn btn-primary"
+            >
+              Next
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
