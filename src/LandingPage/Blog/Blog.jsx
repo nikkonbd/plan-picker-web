6c169366baadb1e58fe94ac69859a1c9e13ecf18
nikkonbd <nikkon1998@gmail.com>
2023-08-29 09:40:34 +0600

    blog details update

--- src/LandingPage/Blog/Blog.jsx
+++ src/LandingPage/Blog/Blog.jsx
@@ -16,14 +16,15 @@ const Blog = () => {
   // blog page
 
   return (
-    <div className="md:w-10/12 mx-auto">
+    <div className="md:w-8/12 mx-auto">
       {/* banner */}
       <div className="md:flex justify-between items-center min-h-[60vh] my-10">
-        <div className="md:w-1/2">
-          <h1 className="text-5xl font-bold">
-            Journey to Success: <br /> Unleash Your Potential Through <br /> Our
-            Articles
+        <div className="md:w-1/2 px-4 md:px-0">
+          <h1 className="md:text-5xl text-3xl font-semibold pb-4">
+            Journey to Success: <br /> <span className="md:text-3xl text-xl">Unleash Your Potential Through <br /> Our
+            Articles</span>
           </h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta eius dignissimos vero natus illum repellendus maxime praesentium pariatur nemo mollitia consequuntur, rerum nostrum dolorem est doloremque earum perferendis facilis architecto minus. Fugiat vitae, quam beatae blanditiis libero, ipsam quia quasi unde, minima laudantium consectetur? Tenetur dolorum animi beatae minima magni! Error, dolorum accusamus.</p>
         </div>
         <div className="md:w-1/2">
           <Lottie animationData={bloganimition} loop={true} />
@@ -31,10 +32,10 @@ const Blog = () => {
       </div>
       {/* article */}
       <div className="my-10">
-        <h1 className="text-center font-semi-bold text-6xl my-5">
+        <h1 className="text-center font-semi-bold md:text-6xl text-3xl my-5">
           Explore our top articles
         </h1>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 md:max-w-6xl lg:max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 px-4 md:px-0 md:grid-cols-3 gap-10 md:max-w-6xl mx-auto">
           {topArticels.map((article) => (
             <BlogCard key={article._id} article={article}></BlogCard>
           ))}
