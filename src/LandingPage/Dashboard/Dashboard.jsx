051a5cf54b9493a73b39ab1028b22947dc344ad0
MostofaAsik <mmostofaasik@gmail.com>
2023-08-30 09:27:46 +0600

    user management

---
+++ src/LandingPage/Dashboard/Dashboard.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+
+function Dashboard() {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    return (
+        <div className="flex h-screen overflow-hidden">
+            {/* Sidebar */}
+            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+            {/* Content area */}
+            <div className="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto">
+                {/*  Site header */}
+                <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+                <main>
+                    <div className="w-full px-4 py-8 mx-auto sm:px-6 lg:px-8 max-w-9xl">
+                        <Outlet />
+                    </div>
+                </main>
+
+                {/* <Banner /> */}
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
+
