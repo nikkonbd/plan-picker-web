efa58c2430ffa1637f7fc6d3bd9c3063c033f125
nikkonbd <nikkon1998@gmail.com>
2023-09-01 03:42:46 +0600

    update section

--- src/LandingPage/Blog/Blog.jsx
+++ src/LandingPage/Blog/Blog.jsx
@@ -20,13 +20,13 @@ const Blog = () => {
       {/* banner */}
       <div className="md:flex justify-between items-center min-h-[60vh] my-10">
         <div className="md:w-1/2 px-4 md:px-0">
-          <h1 className="md:text-5xl text-3xl font-semibold pb-2 text-[#5EBEC4]">
+          <h1 className="md:text-4xl text-2xl font-bold pb-2 text-[#5EBEC4]">
             Journey to Success: <br />{" "}
             <span className="text-base text-[#42a6ad]">
               Unleash Your Potential Through Articles Our Articles
             </span>
           </h1>
-          <p>
+          <p className="text-base">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta eius
             dignissimos vero natus illum repellendus maxime praesentium pariatur
             nemo mollitia consequuntur, rerum nostrum dolorem est doloremque
@@ -43,14 +43,14 @@ const Blog = () => {
       {/* article */}
       <div className="my-10">
         <div className="py-5">
-          <h1 className="text-center font-semi-bold md:text-6xl text-3xl">
+          <h1 className="text-center font-bold md:text-5xl text-2xl">
             Explore our top articles
           </h1>
           <p className="text-center text-base">
             This is our Top article section
           </p>
         </div>
-        <div className="grid grid-cols-1 px-4 md:px-0 md:grid-cols-3 gap-10 md:max-w-6xl mx-auto">
+        <div className="grid grid-cols-1 px-4 md:px-0 md:grid-cols-2 lg:grid-cols-3 gap-10 md:max-w-6xl mx-auto">
           {topArticels.map((article) => (
             <BlogCard key={article._id} article={article}></BlogCard>
           ))}
