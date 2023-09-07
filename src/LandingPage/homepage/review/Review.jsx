5da8ab12cb98eafd15fca147a496b220f1565408
opu183059 <opuctg440@gmail.com>
2023-09-07 13:33:34 +0600

    review secction updated

--- src/LandingPage/homepage/review/Review.jsx
+++ src/LandingPage/homepage/review/Review.jsx
@@ -30,40 +30,52 @@ const Review = () => {
     },
   ];
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 500000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Navigation]}
-        className="mySwiper"
-      >
-        {sliderData.map((item, index) => (
-          <SwiperSlide key={index}>
-            <div className="md:h-[350px] md:w-7/12 w-9/12 bg-teal-50/80 rounded-lg flex md:flex-row flex-col items-center justify-center border-2 border-teal-700/50 md:p-5 p-2">
-              <div className="md:w-5/12 flex items-center justify-center">
-                <div className="round">
-                  <img src={item.img} alt="" className="w-48 h-48 scale-105" />
+    <div className="min-h-screen flex flex-col justify-center">
+      <h2 className="text-xl md:text-4xl text-[#42a6ad] font-semibold text-center">
+        What users say's about us
+      </h2>
+      <div className="mt-10">
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 500000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Navigation]}
+          className="mySwiper"
+        >
+          {sliderData.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div className="md:h-[350px] md:w-7/12 w-9/12 bg-teal-50/80 rounded-lg flex md:flex-row flex-col items-center justify-center border-2 border-teal-700/50 md:p-5 p-2 relative overflow-hidden">
+                <div className="hidden md:block bg-white shadow-lg shadow-teal-800/30 absolute top-9 -left-60 -rotate-[25deg] z-10 w-full text-center py-1">
+                  <p className="font-semibold uppercase">{item.company}</p>
+                </div>
+                <div className="md:w-5/12 flex items-center justify-center py-4">
+                  <div className="rounded-[35px] overflow-hidden rotate-45 w-40 h-40">
+                    <img
+                      src={item.img}
+                      alt=""
+                      className="w-40 h-40 scale-150 -rotate-45 "
+                    />
+                  </div>
+                </div>
+                <div className="md:w-7/12 relative p-3 md:p-0 text-center md:text-left">
+                  <p className="text-teal-700">"{item.review}"</p>
+                  <h3 className="mt-5 font-bold text-black">{item.name}</h3>
+                  <p className="text-teal-800 font-semibold text-xl">
+                    {item.company}
+                  </p>
                 </div>
               </div>
-              <div className="md:w-7/12 relative p-3 md:p-0 text-center md:text-left">
-                <p className="text-teal-700">"{item.review}"</p>
-                <h3 className="mt-5 font-bold text-black">{item.name}</h3>
-                <p className="text-teal-800 font-semibold text-xl uppercase">
-                  {item.company}
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
